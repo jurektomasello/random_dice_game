b3cd6eb82850a692679adc36a5aa3185a66e191f
Jurek <jurek.tomasello@gmail.com>
2021-10-16 13:28:11 +0200

    Dices' images correctly change every time the button is pressed

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ function diceRoll() {
     var playerTwo = Math.floor(Math.random() * 6) + 1;
 
     if (playerOne > playerTwo) {
-
-        document.getElementById("score" + playerOne).style.display = "inline-block";
-        document.getElementById("scoreRight" + playerTwo).style.display = "inline-block";
+        document.getElementById("left-dice-img").src = "images/dice" + playerOne + ".png"; 
+        document.getElementById("right-dice-img").src = "images/dice" + playerTwo + ".png"; 
 
         document.getElementById("winner-announcement").innerHTML = "Player One wins!";
 
     } else if (playerOne === playerTwo) {
+        document.getElementById("left-dice-img").src = "images/dice" + playerOne + ".png"; 
+        document.getElementById("right-dice-img").src = "images/dice" + playerTwo + ".png"; 
         document.getElementById("winner-announcement").innerHTML = "It's a draw!";
+    
     } else {
+        document.getElementById("left-dice-img").src = "images/dice" + playerOne + ".png"; 
+        document.getElementById("right-dice-img").src = "images/dice" + playerTwo + ".png"; 
         document.getElementById("winner-announcement").innerHTML = "Player Two wins!";
     }
 }
 
 document.getElementsByClassName("roll-button")[0].onclick = function () {
     diceRoll();
-
 }
